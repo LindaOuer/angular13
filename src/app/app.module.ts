eed84fbd78d2e7b28d4cd5869d1d43ff8625e410
LindaOuer <linda.ouerfelli@gmail.com>
2022-09-27 11:03:09 +0100

    footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { HeaderComponent } from './header/header.component';
 import { UserCardComponent } from './user-card/user-card.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListUserComponent } from './list-user/list-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
     HeaderComponent,
-    UserCardComponent
+    UserCardComponent,
+    FooterComponent,
+    ListUserComponent
   ],
   imports: [
     BrowserModule,
