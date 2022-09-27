18a1b1045a450af79bcac899256ab64594da7b4c
LindaOuer <linda.ouerfelli@gmail.com>
2022-09-27 10:57:07 +0100

    component example - userCard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { HeaderComponent } from './header/header.component';
+import { UserCardComponent } from './user-card/user-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    HeaderComponent,
+    UserCardComponent
   ],
   imports: [
     BrowserModule,
